a59717d0eda2f02aa97b05f45f455c326a90e619
Rishab260997 <rishab260997@gmail.com>
2020-09-10 12:06:40 +0530

    Updates

---
+++ precache-manifest.baea922015d888c0a28833b04f304ea1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0497f3ab9ab857ac3970fbd17f94decb",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "77654106ae3ad541b263",
+    "url": "/todoapp/static/css/main.b3c9b034.chunk.css"
+  },
+  {
+    "revision": "1979673bd7cb7101276a",
+    "url": "/todoapp/static/js/2.fa2ca534.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.fa2ca534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77654106ae3ad541b263",
+    "url": "/todoapp/static/js/main.4c112cce.chunk.js"
+  },
+  {
+    "revision": "566fce36fccc7e3661a2",
+    "url": "/todoapp/static/js/runtime-main.9b29aac0.js"
+  }
+]);
